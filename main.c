9a861eea75b595fa9d48207403ea74e9cc9cca16
jslusark <slusark.jessica@gmail.com>
2025-01-02 17:19:52 +0100

    added more to parsing error check

--- main.c
+++ main.c
@@ -6,46 +6,59 @@
 /*   By: jslusark <jslusark@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:46:53 by jslusark          #+#    #+#             */
-/*   Updated: 2025/01/02 15:46:16 by jslusark         ###   ########.fr       */
+/*   Updated: 2025/01/02 17:19:21 by jslusark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philos.h"
 
+int check_values(t_data *program)
+{
+	if(program->philos_n < 1 || program->time_to_die < 1 || program->time_to_eat < 1 || program->time_to_sleep < 1 || program->eat_req < 1)
+	{
+		printf("Error: all values should be greater than 0\n"); // check if this also needs to be for the times
+		return(0);
+	}
+	if(program->philos_n > 200)
+	{
+		printf("Error: philos_n should not greater than 200\n");
+		return(0);
+	}
+	// what about intmax limit?
+	return(1);
+}
+
 int	parse_args(int argc, char **argv, t_data *program)
 {
-	if(argc < 5 || argc > 6)
+	if(argc != 5 && argc != 6)
 	{
-		printf("Error: run ./philos philo_n time_to_die time_to_eat time_to_sleep\n");
+		printf("Error: run ./philos philos_n time_to_die time_to_eat time_to_sleep and eats_n(optional)\n");
 		return (0);
 	}
-	printf("philo_n: %d, time_to_die: %d, time_to_eat: %d, time_to_sleep: %d\n", ft_atoi(argv[1]), ft_atoi(argv[2]), ft_atoi(argv[3]), ft_atoi(argv[4]));
-	program->philo_n = ft_atoi(argv[1]);
+	printf("philos_n: %d, time_to_die: %d, time_to_eat: %d, time_to_sleep: %d\n", ft_atoi(argv[1]), ft_atoi(argv[2]), ft_atoi(argv[3]), ft_atoi(argv[4]));
+	program->philos_n = ft_atoi(argv[1]); // i have to hanldle letters? should not give 0??
+	program->forks_n = program->philos_n;
 	program->time_to_die = ft_atoi(argv[2]);
 	program->time_to_eat = ft_atoi(argv[3]);
 	program->time_to_sleep = ft_atoi(argv[4]);
-	program->forks_n = program->philo_n;
+	if (argc == 6)
+		program->eat_req = ft_atoi(argv[5]);
+	else
+		program->eat_req = 0; // what happens when this is not added?
 	return (1);
 }
 
 int main(int argc, char **argv)
 {
-	t_data program;
+	t_data program; // needs to be malloced after parsing
+	// t_philo philo; // needs to be malloced after parsing
 
-	if(!parse_args(argc, argv, &program))
-	{
-		printf("Error: parsing arguments failed! \n");
+	if(!parse_args(argc, argv, &program) || !check_values(&program))
 		return(1);
-	}
-	if(program.philo_n == 0)
-	{
-		printf("Error: philo_n should be greater than 1\n");
-		return(1);
-	}
 	printf("PROGRAM RUNNING...\n");
-	if(program.philo_n == 1) // i guess this has to run as a thread once i handle them
+	if(program.philos_n == 1) // i guess this has to run as a thread once i handle them
 	{
-		printf("%d is dead\n", program.philo_n);
+		printf("%d is dead\n", program.philos_n);
 		return(1);
 	}
 	return(0);
--- philos.h
+++ philos.h
@@ -6,7 +6,7 @@
 /*   By: jslusark <jslusark@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 13:47:09 by jslusark          #+#    #+#             */
-/*   Updated: 2025/01/02 15:54:04 by jslusark         ###   ########.fr       */
+/*   Updated: 2025/01/02 17:15:13 by jslusark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,28 @@
 #include <sys/time.h>   // gettimeofday
 #include <pthread.h>    // pthread_create, pthread_detach , pthread_join, pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
 
-
-struct s_philo
+typedef struct s_philo
 {
-	// data of philo/
-	// each philo should be a thread
+	// data of philo - each philo should be a thread?
+	int id; // index of philo?
+	int eat_counter; // do we still need this when eat_req is not added as argv?
+	// index of prev_fork (philo id - 1)
+	// index of own_fork (philo id)
+	// index of next_fork (philo id + 1)
+	// is_eating flag?
+	// is_dead flag?
+	// is_sleeping flag?
 } t_philo;
 
 typedef	struct s_data
 {
 	// i suppose we will have to store the data of the program here for further use
-	int philo_n;
+	int philos_n;
+	int forks_n;
 	int time_to_die;
 	int time_to_eat;
 	int time_to_sleep;
-	int forks_n;
+	int eat_req; // this is added optionally? what happens when not added?
 }	t_data;
 
 
